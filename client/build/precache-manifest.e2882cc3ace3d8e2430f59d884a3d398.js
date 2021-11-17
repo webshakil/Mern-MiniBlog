2f6f3fd8b9465b56cd603a76a00788e385d03c8a
webshakil <chumkysk@gmail.com>
2021-11-17 08:28:17 +0600

    nodemon configured

---
+++ client/build/precache-manifest.e2882cc3ace3d8e2430f59d884a3d398.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67632480e44ee9bfcbb934c3c70d0c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc8beb102301c7c3875",
+    "url": "/static/css/main.95c04b65.chunk.css"
+  },
+  {
+    "revision": "94722200f2bb2c54f87b",
+    "url": "/static/js/2.32d91e32.chunk.js"
+  },
+  {
+    "revision": "c06d497d6716e84111c2fd9253bcfc02",
+    "url": "/static/js/2.32d91e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efc8beb102301c7c3875",
+    "url": "/static/js/main.4d51490b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "dc8bbfe30b9b1cf8867a83e69d051651",
+    "url": "/static/media/memoriesLogo.dc8bbfe3.png"
+  },
+  {
+    "revision": "2b89c5fae2d163d5a126a519646883fa",
+    "url": "/static/media/memoriesText.2b89c5fa.png"
+  }
+]);
--- index.js
+++ index.js
@@ -2,12 +2,31 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import path from 'path';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
+const __dirname = path.resolve();
+
 
 
 const app = express();
 dotenv.config();
+
+//For deployment
+if ( process.env.NODE_ENV == "production"){
+ 
+  app.use(express.static(path.join(__dirname,"/client/build")));
+
+  app.get("*", (req, res) => {
+
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+  }else{
+      app.get("/", (req, res) => {
+          res.send("Api is Running");
+      });
+  }
+
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
@@ -23,7 +42,7 @@ app.get('/posts',(req,res)=>{
 //const CONNECTION_URL = 'mongodb://localhost:27017/memories';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
